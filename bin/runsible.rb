3e1afc7da850bf2ba044e8e3ed97242ee5ee1f02
Rick Hull <rick.hull@gmail.com>
2015-04-12 21:31:35 -0700

    move the tough stuff from bin to lib

--- bin/runsible.rb
+++ bin/runsible.rb
@@ -6,13 +6,13 @@ require 'runsible'
 
 $opts = Slop.parse do |o|
   o.banner = "usage: runcible.rb [options] yaml_file"
-  o.string '-u' '--user', 'remote user [root]', default: "root"
-  o.string '-H', '--host', 'remote host', default: "127.0.0.1"
-  o.int '-p', '--port', 'remote port [22]', default: 22
-  o.int '-r', '--retries', 'number of retry attempts [0]', default: 0
-  o.string '-m', '--email', 'where to send email alerts'
-  o.bool '-s', '--silent', "suppress alerts"
-  o.string '-v', '--vars', 'list of vars to pass thru, e.g.: "FOO BAR"'
+  o.string '-u', '--user',    "remote user [#{ENV['USER']}]]", default: ENV['USER']
+  o.string '-H', '--host',    'remote host [127.0.0.1]', default: "127.0.0.1"
+  o.int    '-p', '--port',    'remote port [22]',        default: 22
+  o.int    '-r', '--retries', 'number of retry attempts [0]', default: 0
+  o.string '-m', '--email',   'where to send email alerts'
+  o.bool   '-s', '--silent',  'suppress alerts'
+  o.string '-v', '--vars',    'list of vars to pass thru, e.g.: "FOO BAR"'
 end
 
 def alert(to, subject, body)
@@ -26,10 +26,6 @@ end
 yaml_filename = $opts.arguments.shift
 usage("yaml_file is a required argument") if yaml_filename.nil?
 
-# yaml['settings'] provides static defaults
-# $opts can override yaml['settings']
-# yaml['runlist'][n] overrides everything
-
 begin
   yaml = YAML.load_file(yaml_filename)
 rescue YAML::Error
@@ -39,46 +35,19 @@ end
 settings = yaml['settings'] || Hash.new
 runlist = yaml['runlist'] || Array.new
 
+# yaml['settings'] provides static defaults
+# $opts can override yaml['settings']
+# yaml['runlist'][n] overrides everything
+
 # merge $opts into settings
-%w[user host port retries email vars].each { |cfg|
+%w[user host port retries email vars silent].each { |cfg|
   opt = $opts[cfg.to_sym]
   settings[cfg] = opt if opt
 }
-$silent = opts.silent? || !settings['alert']
-vars = settings['vars'] || []
-vars = vars.split if vars.is_a?(String)
 
-# begin SSH session
-Net::SSH.start(settings.fetch('host'),
-               settings.fetch('user'),
-               port: settings.fetch('port'),
-               forward_agent: true,
-               send_env: vars,
-               timeout: 10) { |ssh|
+#if !vars.empty?
+#  Runsible.warn "SSH :send_env does not behave as expected"
+#  Runsible.warn "vars (#{vars.join(' ')}) will be ignored!"
+#end
 
-  # execute runlist with failure handling, retries, alerting, etc.
-  runlist.each { |run|
-    cmd = run.fetch('command')
-    retries = run['retries'] || settings['retries']
-    on_failure = run['on_failure'] || 'exit'
-
-    begin
-      Runsible.retry_ssh(ssh, cmd, retries)
-    rescue CommandFailure, Net::SSH::Exception => e
-      alert(settings['email'], "test_subject", e.to_s)
-      case on_failure
-      when 'exit'
-        exit 1
-      when 'continue'
-        # ok
-      else
-        if yaml[on_failure]
-          warn "on_failure: #{on_failure} found in YAML; not yet supported"
-        else
-          warn "unknown on_failure: #{on_failure}"
-        end
-        exit 1
-      end
-    end
-  }
-}
+Runsible.ssh_runlist(settings, runlist, Hash.new, yaml)
--- lib/runsible.rb
+++ lib/runsible.rb
@@ -1,9 +1,24 @@
 require 'pony'
 require 'net/ssh'
 
-class CommandFailure < RuntimeError; end
+# for nonzero exit status of a remote command
+class CommandFailure < RuntimeError
+  def to_s(*args)
+    "#{self.class}: #{super(*args)}"
+  end
+end
 
+# - this module is deliberately written without private state
+# - it is meant to be used in the helper style
+# - whenever a remote command sends data to STDOUT or STDERR, Runsible will
+#   immediately send it to the corresponding local IO
+# - Runsible itself writes some warnings and timestamped command delimeters to
+#   STDOUT and STDERR
+#
 module Runsible
+  SSH_CNX_TIMEOUT = 10
+  class Error < RuntimeError; end
+
   def self.usage(opts, msg=nil)
     puts opts
     puts
@@ -11,21 +26,83 @@ module Runsible
     exit 1
   end
 
-  def self.email_alert(to, subject, body)
-    Pony.mail(to: to,
-              from: 'runsible@glassdoor.com',
-              subject: subject,
-              body: body)
+  def self.alert(to, subject, body, backend=:email)
+    case backend
+    when :email
+      Pony.mail(to: to,
+                from: 'runsible@spoon',
+                subject: subject,
+                body: body)
+    when :kafka
+      # TODO
+      raise Error, "unsupported backend: #{backend.inspect}"
+    else
+      raise Error, "unknown backend: #{backend.inspect}"
+    end
   end
 
-  # TODO: kafka_alert
+  def self.warn(msg)
+    $stdout.puts msg
+    $stderr.puts msg
+  end
+
+  def self.ssh_runlist(settings, runlist, ssh_options, yaml)
+    ssh_options[:forward_agent] ||= true
+    ssh_options[:port] ||= settings.fetch('port')
+    ssh_options[:send_env] ||= settings['vars'] if settings['vars']
+    ssh_options[:timeout] ||= SSH_CNX_TIMEOUT
+    host, user = settings.fetch('host'), settings.fetch('user')
+    Net::SSH.start(host, user, ssh_options) { |ssh|
+      self.exec_runlist(ssh, runlist, settings, yaml)
+    }
+  end
+
+  def self.die!(msg, alert_to)
+    self.warn(msg)
+    self.alert(alert_to, "Runsible: FATAL #{msg}", msg)
+    exit 1
+  end
+
+  # execute runlist with failure handling, retries, alerting, etc.
+  def self.exec_runlist(ssh, runlist, settings, yaml = Hash.new)
+    runlist.each { |run|
+      cmd = run.fetch('command')
+      retries = run['retries'] || settings['retries']
+      on_failure = run['on_failure'] || 'exit'
+
+      begin
+        self.exec_retry(ssh, cmd, retries)
+      rescue CommandFailure, Net::SSH::Exception => e
+        self.warn e
+        msg = "#{retries} retries exhausted; on_failure: #{on_failure}"
+        self.warn msg
+        self.alert(settings['email'], "retries exhausted", e.to_s)
+
+        case on_failure
+        when 'continue'
+          next
+        when 'exit'
+        else
+          if yaml[on_failure]
+            self.warn "found #{yaml[on_failure]} runlist"
+            # pass empty hash for yaml here to prevent infinite loops
+            self.exec_runlist(ssh, yaml[on_failure], settings, Hash.new)
+            self.warn "exiting failure after #{yaml[on_failure]}"
+          else
+            self.warn "#{on_failure} unknown"
+          end
+        end
+        self.die!("exiting after `#{cmd}` ultimately failed")
+      end
+    }
+  end
 
   # prints remote STDOUT to local STDOUT, likewise for STDERR
   # raises on SSH channel exec failure or nonzero exit status
-  def self.ssh_exec(ssh, cmd)
+  def self.exec(ssh, cmd)
     exit_code = nil
     ssh.open_channel do |channel|
-      channel.exec(command) do |ch, success|
+      channel.exec(cmd) do |ch, success|
         raise(Net::SSH::Exception, "SSH channel exec failure") unless success
         channel.on_data do |ch,data|
           $stdout.puts data
@@ -44,29 +121,30 @@ module Runsible
 
   # retry several times, rescuing CommandFailure
   # raises on SSH channel exec failure and CommandFailure on final retry
-  def self.ssh_retry(ssh, cmd, retries)
+  def self.exec_retry(ssh, cmd, retries)
     self.banner_wrap(cmd) {
       success = false
       retries.times { |i|
         begin
-          success = self.ssh_exec(ssh, cmd)
+          success = self.exec(ssh, cmd)
+          break
         rescue CommandFailure => e
+          $stdout.puts "#{e}; retrying shortly..."
           $stderr.puts e
-          sleep 5
+          sleep 2
         end
-        break if success
       }
       # the final retry, may blow up
-      success or self.ssh_exec(ssh, cmd)
+      success or self.exec(ssh, cmd)
     }
   end
 
   def self.begin_banner(msg)
-    "RUNSIBLE > #{self.timestamp} > #{msg} >>>"
+    "RUNSIBLE >>> [#{self.timestamp}] >>> #{msg} >>>>>"
   end
 
   def self.end_banner(msg)
-    "<<< #{msg} < #{self.timestamp} < RUNSIBLE"
+    "<<<<< #{msg} <<< [#{self.timestamp}] <<< RUNSIBLE"
   end
 
   def self.timestamp(t = Time.now)
@@ -74,9 +152,8 @@ module Runsible
   end
 
   def self.banner_wrap(msg)
-    [$stdout, $stderr].each { |io| io.puts begin_banner(msg) }
+    self.warn self.begin_banner(msg)
     yield if block_given?
-    [$stdout, $stderr].each { |io| io.puts end_banner(msg) }
-    val
+    self.warn self.end_banner(msg)
   end
 end
