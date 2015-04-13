e51f9bcc046f8234144e2e36005567d19d31b247
Rick Hull <rick.hull@gmail.com>
2015-04-13 00:26:29 -0700

    merge Runsible.run into .spoon
    - general organize and polish

--- lib/runsible.rb
+++ lib/runsible.rb
@@ -18,8 +18,8 @@ end
 #   STDOUT and STDERR
 #
 module Runsible
-  SSH_CNX_TIMEOUT = 10
   class Error < RuntimeError; end
+  SSH_CNX_TIMEOUT = 10
 
   SETTINGS = {
     user: ENV['USER'],
@@ -28,6 +28,10 @@ module Runsible
     retries: 0,
     vars: [],
   }
+
+  #
+  # Utility stuff
+  #
 
   def self.default_settings
     hsh = {}
@@ -46,55 +50,6 @@ module Runsible
     puts
     puts msg if msg
     exit 1
-  end
-
-  def self.run(ssh_options = Hash.new)
-    opts = Runsible.slop_parse
-    Runsible.spoon(opts, self.extract_yaml(opts), ssh_options)
-  end
-
-  def self.extract_yaml(opts)
-    yaml_filename = opts.arguments.shift
-    self.usage(opts, "yaml_file is required") if yaml_filename.nil?
-
-    begin
-      yaml = YAML.load_file(yaml_filename)
-    rescue RuntimeError => e
-      Runsible.usage(opts, "could not load yaml_file\n#{e}")
-    end
-    yaml
-  end
-
-  def self.slop_parse
-    d = SETTINGS
-    Slop.parse do |o|
-      o.banner = "usage: runsible [options] yaml_file"
-      o.on '-h', '--help' do
-        puts o
-        exit 0
-      end
-      o.on     '-v', '--version', 'show Runsible version' do
-        puts Runsible.version
-        exit 0
-      end
-
-      o.string '-u', '--user',    "remote user [#{d[:user]}]"
-      o.string '-H', '--host',    "remote host [#{d[:host]}]"
-      o.int    '-p', '--port',    "remote port [#{d[:port]}]"
-      o.int    '-r', '--retries', "retry count [#{d[:retries]}]"
-      o.bool   '-s', '--silent',  'suppress alerts'
-      # this feature does not yet work as expected
-      # https://github.com/net-ssh/net-ssh/issues/236
-      #  o.string '-v', '--vars',    'list of vars to pass, e.g.: "FOO BAR"'
-    end
-  end
-
-  def self.merge(opts, settings)
-    Runsible::SETTINGS.keys.each { |sym|
-      settings[sym.to_s] = opts[sym] if opts[sym]
-    }
-    settings['alerts'] = {} if opts.silent?
-    settings
   end
 
   def self.alert(topic, message, settings)
@@ -120,12 +75,67 @@ module Runsible
     $stderr.puts msg
   end
 
-  def self.spoon(opts, yaml, ssh_options = Hash.new)
+  def self.die!(msg, settings)
+    self.warn(msg)
+    self.alert("runsible:fatal:#{Process.pid}", msg, settings)
+    exit 1
+  end
+
+
+  #
+  # CLI or bin/runsible stuff
+  #
+
+  # bin/runsible entry point
+  def self.spoon(ssh_options = Hash.new)
+    opts = Runsible.slop_parse
+    yaml = self.extract_yaml(opts)
     settings = Runsible.default_settings.merge(yaml['settings'] || Hash.new)
     settings = self.merge(opts, settings)
     self.ssh_runlist(settings, yaml['runlist'] || Array.new, ssh_options, yaml)
   end
 
+  def self.slop_parse
+    d = SETTINGS # display defaults
+    Slop.parse do |o|
+      o.banner = "usage: runsible [options] yaml_file"
+      o.on '-h', '--help' do
+        puts o
+        exit 0
+      end
+      o.on     '-v', '--version', 'show Runsible version' do
+        puts Runsible.version
+        exit 0
+      end
+
+      o.string '-u', '--user',    "remote user [#{d[:user]}]"
+      o.string '-H', '--host',    "remote host [#{d[:host]}]"
+      o.int    '-p', '--port',    "remote port [#{d[:port]}]"
+      o.int    '-r', '--retries', "retry count [#{d[:retries]}]"
+      o.bool   '-s', '--silent',  'suppress alerts'
+      # this feature does not yet work as expected
+      # https://github.com/net-ssh/net-ssh/issues/236
+      #  o.string '-v', '--vars',    'list of vars to pass, e.g.: "FOO BAR"'
+    end
+  end
+
+  def self.extract_yaml(opts)
+    yaml_filename = opts.arguments.shift
+    self.usage(opts, "yaml_file is required") if yaml_filename.nil?
+
+    begin
+      yaml = YAML.load_file(yaml_filename)
+    rescue RuntimeError => e
+      Runsible.usage(opts, "could not load yaml_file\n#{e}")
+    end
+    yaml
+  end
+
+  #
+  # Library stuff
+  #
+
+  # initiate ssh connection, perform the runlist
   def self.ssh_runlist(settings, runlist, ssh_options, yaml)
     ssh_options[:forward_agent] ||= true
     ssh_options[:port] ||= settings.fetch('port')
@@ -135,12 +145,6 @@ module Runsible
     Net::SSH.start(host, user, ssh_options) { |ssh|
       self.exec_runlist(ssh, runlist, settings, yaml)
     }
-  end
-
-  def self.die!(msg, settings)
-    self.warn(msg)
-    self.alert("runsible:fatal:#{Process.pid}", msg, settings)
-    exit 1
   end
 
   # execute runlist with failure handling, retries, alerting, etc.
@@ -177,6 +181,26 @@ module Runsible
     }
   end
 
+  # retry several times, rescuing CommandFailure
+  # raises on SSH channel exec failure and CommandFailure on final retry
+  def self.exec_retry(ssh, cmd, retries)
+    self.banner_wrap(cmd) {
+      success = false
+      retries.times { |i|
+        begin
+          success = self.exec(ssh, cmd)
+          break
+        rescue CommandFailure => e
+          $stdout.puts "#{e}; retrying shortly..."
+          $stderr.puts e
+          sleep 2
+        end
+      }
+      # the final retry, may blow up
+      success or self.exec(ssh, cmd)
+    }
+  end
+
   # prints remote STDOUT to local STDOUT, likewise for STDERR
   # raises on SSH channel exec failure or nonzero exit status
   def self.exec(ssh, cmd)
@@ -199,24 +223,17 @@ module Runsible
     exit_code == 0 or raise(CommandFailure, "[exit #{exit_code}] #{cmd}")
   end
 
-  # retry several times, rescuing CommandFailure
-  # raises on SSH channel exec failure and CommandFailure on final retry
-  def self.exec_retry(ssh, cmd, retries)
-    self.banner_wrap(cmd) {
-      success = false
-      retries.times { |i|
-        begin
-          success = self.exec(ssh, cmd)
-          break
-        rescue CommandFailure => e
-          $stdout.puts "#{e}; retrying shortly..."
-          $stderr.puts e
-          sleep 2
-        end
-      }
-      # the final retry, may blow up
-      success or self.exec(ssh, cmd)
+
+  #
+  # Necessities
+  #
+
+  def self.merge(opts, settings)
+    Runsible::SETTINGS.keys.each { |sym|
+      settings[sym.to_s] = opts[sym] if opts[sym]
     }
+    settings['alerts'] = {} if opts.silent?
+    settings
   end
 
   def self.begin_banner(msg)
